78e0c2ffb313d382251516808c42d63119cc56b3
TanglyEagle7718 <74271243+TanglyEagle7718@users.noreply.github.com>
2022-02-06 18:31:35 -0500

    Updated validation and partial data sets

--- main.py
+++ main.py
@@ -15,46 +15,64 @@ import numpy as np
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
 
+"""
+Get data from already cleaned csv file
+"""
 fileName = 'new_stroke_detection_data.csv'
 print("fileName: ", fileName)
 raw_data = open(fileName, 'rt')
 data = np.loadtxt(raw_data, usecols = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18),
                   delimiter=",")
-
 x = data[:, :18]
 y = data[:, 18]
 
 
-x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, train_size=0.75,test_size=0.25, random_state=101)
+"""
+Split data into training and test data
+"""
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x
+                , y, train_size=0.75,test_size=0.25, random_state=101)
 
-
-(train_data, train_targets), (test_data, test_targets) = (x_train, y_train), (x_test, y_test)
-
-
-
+"""
+Builds network
+"""
 model = models.Sequential()
 model.add(layers.Dense(64, activation='relu', input_shape = (1,18) ))
-model.add(layers.Dense(44, activation='relu'))
+model.add(layers.Dense(44, activation='sigmoid'))
+model.add(layers.Dense(30, activation="tanh"))
 model.add(layers.Dense(1))
 
+
+"""
+Configures model
+"""
 from keras import optimizers
 
 model.compile(optimizer=optimizers.RMSprop(lr=0.001),
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-x_val = x_train[:18]
-partial_x_train = x_train[18:]
+"""
+Builds validation data
+"""
+###Most confusing of them all
+#Total over 3000 data entries
+x_val = x_train[:1500]
+partial_x_train = x_train[1500:]
 
-y_val = y_train[:18]
-partial_y_train = y_train[18:]
+y_val = y_train[:1500]
+partial_y_train = y_train[1500:]
+#Ask Mr. Chapin bout above stuff, and why accuracy and loss are always constant
 
-history = model.fit(partial_x_train, partial_y_train, epochs=50, batch_size=1, validation_data=(x_val, y_val))
+"""
+Trains data
+"""
+history = model.fit(partial_x_train, partial_y_train, epochs=10, batch_size=1, validation_data=(x_val, y_val))
 
-#mae_history = history.history['mae']
-test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
-predicted_prices = model.predict(test_data)
 
+"""
+Plots results
+"""
 results = model.evaluate(partial_x_train, partial_y_train)
 print ("train:", results)
 results = model.evaluate(x_val, y_val)
@@ -74,19 +92,6 @@ loss = history.history['loss']
 val_loss = history.history['val_loss']
 
 epochs = range(1, len(acc) + 1)
-
-# =============================================================================
-# # "bo" is for "blue dot"
-# plt.plot(epochs, loss, 'bo', label='Training loss')
-# # b is for "solid blue line"
-# plt.plot(epochs, val_loss, 'b', label='Validation loss')
-# plt.title('Training and validation loss')
-# plt.xlabel('Epochs')
-# plt.ylabel('Loss')
-# plt.legend()
-# 
-# plt.show()
-# =============================================================================
 
 """
 plot accuracy
