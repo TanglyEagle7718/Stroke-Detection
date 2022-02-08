8614902c06c1ab860a165e7ca49684251e2a7019
TanglyEagle7718 <74271243+TanglyEagle7718@users.noreply.github.com>
2022-02-08 12:17:29 -0500

    Update

--- main3.py
+++ main3.py
@@ -39,19 +39,21 @@ Builds network
 model = models.Sequential()
 model.add(layers.Dense(18, activation='relu', input_shape = (18,) ))
 model.add(layers.Dense(9, activation='relu'))
-model.add(layers.Dense(1))
+model.add(layers.Dense(1, activation='sigmoid'))
+
 
 """
 Configures model
 """
-x_val = x_train[:1500]
-partial_x_train = x_train[1500:]
 
-y_val = y_train[:1500]
-partial_y_train = y_train[1500:]
+x_val = x_train[:400]
+partial_x_train = x_train[400:]
 
-model.compile(optimizer=optimizers.RMSprop(lr=0.005), loss='mse', metrics=['mae','accuracy'])
-history = model.fit(partial_x_train, partial_y_train, epochs= 10, batch_size=1, validation_data=(x_val,y_val))
+y_val = y_train[:400]
+partial_y_train = y_train[400:]
+
+model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='mse', metrics=['mae','accuracy'])
+history = model.fit(partial_x_train, partial_y_train, epochs= 50, batch_size=1024, validation_data=(x_val,y_val))
 mae_history = history.history['mae']
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
@@ -73,7 +75,7 @@ ax.set(xlabel='Epochs', ylabel='Loss',
 ax.legend()
 
 fig1, ax1 = plt.subplots()
-ax1.set_ylim(0.94, 0.99)
+ax1.set_ylim(0.75, 0.99)
 ax1.plot(epochs, acc, 'ro', label='Training acc')
 ax1.plot(epochs, val_acc, 'b', label='Validation acc')
 ax1.set(xlabel='Epochs', ylabel='Accuracy', title='Training and validation accuracy');
@@ -82,3 +84,44 @@ ax1.legend()
 plt.show()
 
 print ("test_mae_score : " + str(test_mae_score))
+
+def comments():
+    # =============================================================================
+    # model = models.Sequential()
+    # model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+    # model.add(layers.Dense(16, activation='relu'))
+    # model.add(layers.Dense(1, activation='sigmoid'))
+    # 
+    # =============================================================================
+    """
+    TRAIN
+    We will now train our model for 20 epochs (20 iterations over all samples in
+     the x_train and y_train tensors), in mini-batches of 512 samples. At this
+     same time we will monitor loss and accuracy on the 10,000 samples that we
+     set apart. This is done by passing the validation data as the validation_data
+     argument:
+    history = model.fit(partial_x_train,
+                        partial_y_train,
+                        epochs=20,
+                        batch_size=512,
+                        validation_data=(x_val, y_val))
+
+
+
+    RESULTS
+    This object has a member history, which is a dictionary containing data about
+     everything that happened during training
+    It contains 4 entries: one per metric that was being monitored, during 
+    training and during validation.
+
+    results = model.evaluate(partial_x_train, partial_y_train)
+    print ("train:", results)
+    results = model.evaluate(x_val, y_val)
+    print ("validation:", results)
+    results = model.evaluate(x_test, y_test)
+    print ("all data", results)
+
+    history_dict = history.history
+    print("history dict.keys():", history_dict.keys())
+
+    """
